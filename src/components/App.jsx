ce46276114211330a02c5cc26d7cacf79f2b2411
Kate Yankovenko <k.yankovenko@ukr.net>
2022-07-25 20:39:39 +0200

    user page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
+import "./App.css";
+import user from "./Profile/user.json";
+
+import Profile from "./Profile/profile";
+
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        maxWidth: "1200px",
+        marginLeft: "auto",
+        margin: "32px auto 64px",
+        alignItems: "center",
+        width: "400px",
+        padding: "15px",
+        borderRadius: "5px",
+        boxShadow: "rgb(197 232 252) 2px 3px 15px 1px"
       }}
     >
-      React homework template
-    </div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      </div>
   );
 };
+
+// function App() {
+//   return <div className="App">React Page</div>;
+// }
+// export default App;
